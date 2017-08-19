84b255fcc2efa505c311054ef52d8e7184e6b34e
Aditya Bhardwaj <30766241+wpboi@users.noreply.github.com>
2017-08-19 20:31:07 +0530

    Warmup - https://goo.gl/huDSWo

---
+++ Staircase.js
@@ -0,0 +1,37 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    for(var i=0; i<n ; i++)
+        {
+            var str = [];
+            for(var j = 0;j<n;j++)
+                {
+                    if(j<n-1-i)
+                        str.push(" ");
+                    else
+                        str.push("#")
+                }
+            console.log(str.join(""));
+        }
+}
