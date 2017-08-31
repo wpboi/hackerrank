c84c3aebd72e1e551c597f30247e2585930d0ee7
Aditya Bhardwaj <30766241+wpboi@users.noreply.github.com>
2017-08-31 23:35:33 +0530

    Ques : https://goo.gl/87JATk

---
+++ kangaroo.js
@@ -0,0 +1,49 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function kangaroo(x1, v1, x2, v2) {
+    // Complete this function
+    if((x1>x2&&v1>v2)||(x1<x2&&v1<v2))
+        return "NO";
+    else if(v1==v2)
+        return "NO";
+    else
+        while(x1<x2) {
+            x1+=v1;
+            x2+=v2;
+            if(x1==x2) return "YES";
+            else
+                continue;
+        }
+    return "NO";
+}
+
+function main() {
+    var x1_temp = readLine().split(' ');
+    var x1 = parseInt(x1_temp[0]);
+    var v1 = parseInt(x1_temp[1]);
+    var x2 = parseInt(x1_temp[2]);
+    var v2 = parseInt(x1_temp[3]);
+    var result = kangaroo(x1, v1, x2, v2);
+    process.stdout.write("" + result + "\n");
+
+}
